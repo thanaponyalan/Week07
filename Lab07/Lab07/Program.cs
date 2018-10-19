cff768d5f60756d48889bd89c7f0a70d89c6e94a
thanaponyalan <thanaponyalan@gmail.com>
2018-10-19 16:43:13 +0700

    static Operator overloading

--- Lab07/Lab07/Program.cs
+++ Lab07/Lab07/Program.cs
@@ -4,30 +4,41 @@ namespace Lab07
 {
     class Program
     {
-        void print(int i)
-        {
-            Console.WriteLine("Printing int: {0}", i);
-        }
-
-        void print(double f)
-        {
-            Console.WriteLine("Printing float: {0}", f);
-        }
-
-        void print(string s)
-        {
-            Console.WriteLine("Printing string: {0}", s);
-        }
-
         static void Main(string[] args)
         {
-            Program p = new Program();
-            p.print(5);
-            p.print((5.26f).ToString());
-            p.print(264.2871);
-            p.print((int)1354.23);
-            p.print("Hello World of C#");
+            Vector v1 = new Vector(1, 4);
+            Vector v2 = new Vector(8, 5);
+            Vector v3 = v1 + v2;
+            Console.WriteLine("vector v1 = " + v1);
+            Console.WriteLine("vector v2 = " + v2);
+            Console.WriteLine("vector v3 = " + v3);
+            Console.WriteLine("-v3 = " + (-v3));
             Console.ReadKey();
         }
     }
+    class Vector
+    {
+        public Vector(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public int X { get; set; }  // X - accessor (property)
+        public int Y { get; set; }  // Y - accessor (property)
+
+        public override string ToString()
+        {
+            return string.Format("({0},{1})", X, Y);
+        }
+        // All operator overloads must be public and static.
+        public static Vector operator +(Vector v1, Vector v2) //binary operator +
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
+        }
+        public static Vector operator -(Vector v1)  //unary operator -
+        {
+            return new Vector(-v1.X, -v1.Y);
+        }
+    }
+
 }
