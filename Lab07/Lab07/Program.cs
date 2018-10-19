42771227bcb3edfb79bf9d7766a60c56595eb401
thanaponyalan <thanaponyalan@gmail.com>
2018-10-19 14:48:09 +0700

    Create class from example

--- Lab07/Lab07/Program.cs
+++ Lab07/Lab07/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab07
 {
@@ -10,6 +6,30 @@ namespace Lab07
     {
         static void Main(string[] args)
         {
+            Point myPoint = new Point();  // create new object
+            myPoint.X = 10;
+            myPoint.Y = 10;
+            Console.WriteLine("myPoint.x = {0}", myPoint.X);
+            Console.WriteLine("myPoint.y = {0}", myPoint.Y);
+            Console.ReadLine();
+        }
+    }
+
+    class Point          // class name
+    {
+        private int x;    // fields
+        private int y;
+
+        public int X      // properties
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
         }
     }
 }
