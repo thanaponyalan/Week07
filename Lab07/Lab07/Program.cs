8016b6ead7d6a6516bbf9ec5847deb2613981e93
thanaponyalan <thanaponyalan@gmail.com>
2018-10-19 16:52:26 +0700

    dynamic

--- Lab07/Lab07/Program.cs
+++ Lab07/Lab07/Program.cs
@@ -6,39 +6,43 @@ namespace Lab07
     {
         static void Main(string[] args)
         {
-            Vector v1 = new Vector(1, 4);
-            Vector v2 = new Vector(8, 5);
-            Vector v3 = v1 + v2;
-            Console.WriteLine("vector v1 = " + v1);
-            Console.WriteLine("vector v2 = " + v2);
-            Console.WriteLine("vector v3 = " + v3);
-            Console.WriteLine("-v3 = " + (-v3));
+            Person p = new Person();
+            p.SayHi();
+            Student s = new Student();
+            s.SayHi();
+            Person sp = new Student();
+            sp.SayHi();
+            Teacher t = new Teacher();
+            t.SayHi();
+            Person tp = new Teacher();
+            tp.SayHi();
             Console.ReadKey();
         }
     }
-    class Vector
-    {
-        public Vector(int x, int y)
-        {
-            this.X = x;
-            this.Y = y;
-        }
-        public int X { get; set; }  // X - accessor (property)
-        public int Y { get; set; }  // Y - accessor (property)
 
-        public override string ToString()
+    class Person
+    {
+        public virtual void SayHi()
         {
-            return string.Format("({0},{1})", X, Y);
-        }
-        // All operator overloads must be public and static.
-        public static Vector operator +(Vector v1, Vector v2) //binary operator +
-        {
-            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
-        }
-        public static Vector operator -(Vector v1)  //unary operator -
-        {
-            return new Vector(-v1.X, -v1.Y);
+            Console.WriteLine("Hi I'm a person");
         }
     }
+
+    class Student : Person
+    {
+        public override void SayHi()
+        {
+            Console.WriteLine("Hi I'm a student");
+        }
+    }
+
+    class Teacher : Person
+    {
+        public new void SayHi()
+        {
+            Console.WriteLine("Hi I'm a teacher");
+        }
+    }
+
 
 }
