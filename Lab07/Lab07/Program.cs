1f4039c90a63d142e2703cbf443a79a6c77b0155
thanaponyalan <thanaponyalan@gmail.com>
2018-10-19 16:40:47 +0700

    static Method overloading

--- Lab07/Lab07/Program.cs
+++ Lab07/Lab07/Program.cs
@@ -4,67 +4,30 @@ namespace Lab07
 {
     class Program
     {
+        void print(int i)
+        {
+            Console.WriteLine("Printing int: {0}", i);
+        }
+
+        void print(double f)
+        {
+            Console.WriteLine("Printing float: {0}", f);
+        }
+
+        void print(string s)
+        {
+            Console.WriteLine("Printing string: {0}", s);
+        }
+
         static void Main(string[] args)
         {
-            Teacher teacher = new Teacher("Tom", 350f);
-            Professor prof = new Professor("Jerry", 500f, 5000f);
-            // teacher work 20Hr/month
-            Console.WriteLine("{0} charge = {1}", teacher.TypeName(),
-                teacher.CalculateCharge(20f));
-            // professor work 12Hr/month
-            Console.WriteLine("{0} charge = {1}", prof.TypeName(),
-                prof.CalculateCharge(12F));
-            Console.ReadLine();
+            Program p = new Program();
+            p.print(5);
+            p.print((5.26f).ToString());
+            p.print(264.2871);
+            p.print((int)1354.23);
+            p.print("Hello World of C#");
+            Console.ReadKey();
         }
     }
-    class Teacher
-    {
-        // constructor (for initial private/protected variables)
-        public Teacher(string name, float billingRate)
-        {
-            this.name = name;
-            this.billingRate = billingRate;
-        }
-        // figure out the charge based on teacher's rate
-        public float CalculateCharge(float hours)
-        {
-            return (hours * billingRate);
-        }
-        // return the name of this type
-        public string TypeName()
-        {
-            return ("Teacher");
-        }
-        private string name;
-        protected float billingRate;
-    }
-    class Professor : Teacher
-    {
-        private float emolument;  // เงินประจำตำแหน่ง
-
-        public Professor(string name, float billingRate) : base(name, billingRate)
-        {
-        }
-
-        public Professor(string name, float billingRate, float emolument)
-        : this(name, billingRate)
-        {
-            this.emolument = emolument;
-        }
-
-        // new function, because it's different than the base version
-        public new float CalculateCharge(float hours)
-        {
-            if (hours < 1.0F)
-                hours = 1.0F; // minimum charge.
-            return (hours * billingRate) + emolument;
-        }
-        // new function, because it's different than the base version
-        public new string TypeName()
-        {
-            return ("Professor");
-        }
-    }
-
-
 }
